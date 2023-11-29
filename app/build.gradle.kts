12e03db8ac4a65e8e597a9bcebad23dc63188fed
Gustavo Fontana <gustavoasf77@gmail.com>
2023-11-29 20:38:28 -0300

    add internet connection and new plugins

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,6 +36,8 @@ android {
 
 dependencies {
 
+    implementation ("com.squareup.retrofit2:retrofit:2.1.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.1.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
